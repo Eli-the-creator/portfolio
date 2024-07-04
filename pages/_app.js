23bd972d574e66b4d4961c29c861743a11b7350b
ELI-THE_CREATOR <neverlookbackeli@gmail.com>
2024-07-04 17:42:19 +0100

    -last gtag

--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,7 @@ import { ThemeProvider } from "next-themes";
 const App = ({ Component, pageProps }) => {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-SH8YG7M3PV"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-SH8YG7M3PV');
-          `}
-      </Script>
+      <GoogleTagManager gtmId="G-SH8YG7M3PV" />
 
       <ThemeProvider>
         <Component {...pageProps} />
